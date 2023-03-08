139a313e047264f1181b0f56b88c2d21dce7daa9
swkim <ap2006kr@naver.com>
2023-03-08 06:04:40 +0000

    update

--- components/score/score.py
+++ components/score/score.py
@@ -1,8 +1,11 @@
 import argparse
 from pathlib import Path
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,mean_squared_error, r2_score
 import os
 import mlflow
+import pandas as pd
+
+mlflow.sklearn.autolog()
 
 parser = argparse.ArgumentParser("score")
 parser.add_argument("--model_input", type=str, help="Path of input model")
@@ -25,6 +28,25 @@ for line in lines:
 # Load the model from input port
 pathmodel = os.path.join(args.model_input, "trained_model")
 model = mlflow.sklearn.load_model(pathmodel)
+
+testfiles = os.listdir(args.test_data)
+os.path.join(args.test_data, testfiles[0])
+# paths are mounted as folder, therefore, we are selecting the file from folder
+test_df = pd.read_csv(os.path.join(args.test_data, testfiles[0]))
+
+# Extracting the label column
+y_test = test_df.pop("default payment next month")
+
+# convert the dataframe values to array
+X_test = test_df.values
+
+y_pred = model.predict(X_test)
+
+# The mean squared error
+print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+# The coefficient of determination: 1 is perfect prediction
+print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+print("Model: ", model)
 
 # Print the results of scoring the predictions against actual values in the test data
 # The coefficients
